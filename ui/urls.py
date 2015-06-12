b00c3da2b88ba3c18c141fe958f87c2b9bb66d45
Juha Yrjölä <juha.yrjola@iki.fi>
2015-06-12 15:21:21 +0300

    Fetch JWT for Linked Events

--- ui/urls.py
+++ ui/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
+from .views import HomeView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="home.html"))
+    url(r'^$', HomeView.as_view())
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import requests
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_le_token(self):
+        LE_CLIENT_ID = 'NWOWjp8ik6YuvyGHSX26BgkL34FBKvQyHuSWlaSM'
+        if not self.request.user.is_authenticated():
+            return None
+        token = self.request.user.socialaccount_set\
+            .get(provider='helsinki').socialtoken_set.first()
+        headers = {'Authorization': 'Bearer %s' % token.token}
+        params = {'target_app': LE_CLIENT_ID}
+        resp = requests.get('https://api.hel.fi/sso/jwt-token/',
+                            headers=headers, params=params)
+        if resp.status_code != 200:
+            raise Exception('JWT token request returned %d' % resp.status_code)
+        return resp.json()['token']
+
+    def get_context_data(self, **kwargs):
+        ret = super(HomeView, self).get_context_data(**kwargs)
+        ret['jwt_token'] = self.get_le_token()
+        return ret
